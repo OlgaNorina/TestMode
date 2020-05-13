8bfa202c762007b1a67f456df8e51550f7b52782
Olga <norinaoa@mail.ru>
2020-05-13 23:42:16 +0500

    test was fixed

--- src/test/java/RegistrationDto.java
+++ src/test/java/RegistrationDto.java
@@ -2,12 +2,14 @@ import com.github.javafaker.Faker;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.Locale;
 
 @Data
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class RegistrationDto {
     private String login;
     private String password;
